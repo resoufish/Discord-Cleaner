101a362e4a85ae2c3de3e927e010b1606868ade3
soufish <drtovski@gmail.com>
2025-08-17 12:57:04 +0500

    add init files

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    outDir: 'dist/vite'
+  },
+})
